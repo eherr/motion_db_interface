816d5bc7ac35998fd0abfcdbe251a0b25fed75ab
Erik Herrmann <Erik.Herrmann@dfki.de>
2022-05-12 16:04:51 +0200

    Removed morpablegraphs dependency by moving functions back to morphablegraphs

--- motion_db_interface/__init__.py
+++ motion_db_interface/__init__.py
@@ -3,11 +3,7 @@ try:
     from .common import authenticate
     from .skeleton_db_interface import *
     from .motion_db_interface import *
-    try:
-        from model_db_interface import *
-    except:
-        print("Warning: morphablegraphs is not installed")
-        pass
+    from .model_db_interface import *
 except:
     print("Error: anim_utils is not installed")
     pass
