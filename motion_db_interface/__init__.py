2b72f241ff3e1d4445cccfce318271767edf8c3b
Erik Herrmann <erik.herrmann@dfki.de>
2022-11-25 18:20:33 +0100

    Removed try catch

--- motion_db_interface/__init__.py
+++ motion_db_interface/__init__.py
@@ -1,10 +1,7 @@
 
-try:
-    from .common import authenticate
-    from .skeleton_db_interface import *
-    from .motion_db_interface import *
-    from .model_db_interface import *
-except:
-    print("Error: anim_utils is not installed")
-    pass
-    
+
+from .common import authenticate
+from .skeleton_db_interface import *
+from .motion_db_interface import *
+from .model_db_interface import *
+
